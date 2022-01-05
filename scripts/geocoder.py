3c3b239bb8078abc8acb1cc1aef8e519a8485a36
Roy Williams <roy.williams.iii@gmail.com>
2022-01-05 00:22:43 -0500

    Add scrapper script

---
+++ scripts/geocoder.py
@@ -0,0 +1,69 @@
+from contextlib import contextmanager
+import json
+import os
+import urllib.parse
+from dataclasses import dataclass, field
+from pathlib import Path
+from typing import ContextManager, Dict, Iterator, Tuple, TypeAlias
+
+import httpx
+from pydantic.main import BaseModel
+
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+GOOGLE_GEOCODE_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
+CACHE_FILE = Path(".cache")
+
+
+class Location(BaseModel):
+    lat: float
+    lng: float
+    place_id: str
+
+
+@dataclass
+class CachingGeocoder:
+    client: httpx.AsyncClient
+    _cache: Dict[str, str] = field(default_factory=dict)
+
+    async def get_location(
+        self,
+        address1: str,
+        address2: str | None,
+        city: str,
+        state_code: str,
+        zip_code: str,
+    ) -> Location | None:
+        params = (
+            (
+                "address",
+                f"{address1}{', ' + address2 if address2 else ''}, {city}, {state_code} {zip_code}",
+            ),
+        )
+        cache_key = urllib.parse.urlencode(params)
+
+        if cache_key not in self._cache:
+            query_params = params + (("key", GOOGLE_API_KEY),)
+            self._cache[cache_key] = (
+                await self.client.get(GOOGLE_GEOCODE_BASE_URL, params=query_params)
+            ).text
+        response = self._cache[cache_key]
+        data = json.loads(response)
+
+        if data["status"] == "OK":
+            result = data["results"][0]
+            location = result["geometry"]["location"]
+            return Location(
+                lat=location["lat"], lng=location["lng"], place_id=result["place_id"]
+            )
+        else:
+            return None
+
+
+@contextmanager
+def Geocoder(client: httpx.AsyncClient) -> Iterator[CachingGeocoder]:
+    saved_cache: Dict[str, str] = {}
+    if CACHE_FILE.exists():
+        saved_cache = json.loads(CACHE_FILE.read_text())
+    yield CachingGeocoder(client, saved_cache)
+    CACHE_FILE.write_text(json.dumps(saved_cache))
